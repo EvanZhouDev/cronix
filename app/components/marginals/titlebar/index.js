bae3296261aeaf5c0af7189e1b5ff86a8a009002
EvanZhouDev <evanzhouyifan@gmail.com>
2023-05-07 20:05:41 -0700

    ✨ Made titlebar more easily used

--- app/components/marginals/titlebar/index.js
+++ app/components/marginals/titlebar/index.js
@@ -5,10 +5,12 @@ import Option from "./option"
 import { FiClock, FiHelpCircle, FiBarChart2 } from "react-icons/fi";
 import styles from "./titlebar.module.css"
 import Session from "./session"
+import Link from "next/link"
+
 export default function Titlebar() {
     return (
         <div className={marginal}>
-            <span className={styles.title}>CRONIX</span>
+            <Link href="/" className={styles.title}>CRONIX</Link>
             <Option icon=<FiClock size={30} /> name={"Timer"} link="/" />
             <Option icon=<FiBarChart2 size={30} /> name={"Statistics"} link="/stats" />
             <Option icon=<FiHelpCircle size={30} /> name={"Help"} link="/help" />
--- app/components/marginals/titlebar/option.js
+++ app/components/marginals/titlebar/option.js
@@ -1,10 +1,15 @@
 'use client';
 import styles from "./titlebar.module.css"
 import Link from "next/link"
-export default function Option({ name, icon, link="/" }) {
+import { usePathname } from 'next/navigation';
+import classNames from "classnames";
+export default function Option({ name, icon, link = "/" }) {
+    let pathname = usePathname()
     return (
         <Link href={link} className={styles.option}>
-            <span className={styles.icon}>
+            <span className={classNames(styles.icon, {
+                [styles.active]: pathname === link
+            })}>
                 {icon}
             </span>
             <div className={styles.dropdown}>

00cdaa6c6f0350d7bfcaac037174992c06f892bc
EvanZhouDev <evanzhouyifan@gmail.com>
2023-06-05 11:56:36 +0800

    🎨 Fixed imports format

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,13 @@ import { Providers } from "@redux/provider"
 import Footer from "./components/marginals/footer"
 import Titlebar from "./components/marginals/titlebar"
 import { Toaster } from 'react-hot-toast';
+import { Fira_Code } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Cronix Timer',
   description: 'The Next-Generation Timer, Powered by Next-Generation Software',
 }
-import { Fira_Code } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
 
 const firacode = Fira_Code({
   weight: ['400', '500', '700'],

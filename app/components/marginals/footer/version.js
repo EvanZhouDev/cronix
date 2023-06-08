60e722ab0ccc1988b2115c95c39a7c651f0529ce
EvanZhouDev <evanzhouyifan@gmail.com>
2023-06-08 09:32:07 +0800

    Bumped version

--- app/components/marginals/footer/version.js
+++ app/components/marginals/footer/version.js
@@ -5,7 +5,7 @@ export default function Version() {
     return (
         <span className={styles.version}>
             <FiGitMerge size={20} />
-            <span className={styles.name}>0.1.0</span>
+            <span className={styles.name}>0.2.0</span>
         </span>
     )
 }

4230fb5d2806373124ef33ee6dd7ae0d74f15bf6
EvanZhouDev <evanzhouyifan@gmail.com>
2023-06-10 19:58:45 +0800

    ✨ Added all-new help page

--- app/(others)/help/page.js
+++ app/(others)/help/page.js
@@ -1,44 +1,58 @@
-import styles from "./help.module.css"
+'use client'
+import React, { useEffect } from 'react';
+import styles from './help.module.css';
+import Gate from "@redux/gate"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './react-tabs-custom.css';
+import useSettings from '@app/redux/accessors/useSettings';
+import { setHelpTab } from '@app/redux/slices/sessions/settings';
+import { useDispatch } from 'react-redux';
+import GeneralTab from './tabs/generalTab';
+import MainPageTab from './tabs/mainPageTab';
+import SessionsTab from "./tabs/sessionsTab";
+import SettingsTab from "./tabs/settingsTab";
+import StatisticsTab from "./tabs/statisticsTab";
+import WidgetsTab from "./tabs/widgetsTab";
 export default function Page() {
+    let settings = useSettings()
+    let dispatch = useDispatch()
     return (
-        <div className={styles.helpPage}>
-            <h1>Cronix Help Page</h1>
-            Welcome to Cronix. If you haven&rsquo;t already, and are interested, read the &ldquo;About&rdquo; page in order to better understand the philosophy behind the timer, and why I built it. However, here is the quick-start guide:
-            <br/>
-            <h3>Timer Page:</h3>
-            <h4>Starting the Timer:</h4>
-            For Timer mode, simply hold down the spacebar, until it shows a yellow time. At this moment, you can release space, and your timer will start. In order to stop the timer, simply press any reasonable key on your keyboard, and the timer will stop.
-            Stackmat mode is currently under development.
-
-            <h4>Choosing your event:</h4>
-            First, choose your event. The whole range of WCA events are available to you, from 2x2-7x7, all of the various other puzzles like Skewb, Pyraminx, and others, to MBLD and 5x5BLD. Simply click on the one you want, and it will be remembered in the browser storage, and persist through your Session.
-
-            <h4>Choosing your timer input:</h4>
-            Cronix currently supports timing via spacebar, and will soon support timing via Stackmat. Again, simply click on the input format you want, and it will persist permanently, even through Sessions.
-
-            <h4>Penalties and time deletion:</h4>
-            If you wish to give yourself a penalty for whatever reason, after your solve, a penalty bar will appear under your time. Click &ldquo;OK&rdquo; to mark your time as fine, &ldquo;+2&rdquo; to add two seconds to your solve, and &ldquo;DNF&rdquo; to mark your solve as not finished. You may also delete your time if you wish to do so.
-
-            <h4>Sessions:</h4>
-            Currently, sessions can be switched on the top of the page. It displays the current session at the right-most position on the titlebar. Hover over it, and you can add sessions, change your session, and delete sessions.
-
-            Nearly everything is localized in sessions. The scramble and time that you have currently will be saved until you visit the session again. This means that the session will be <i>exactly</i> the same as you left it.
-
-            <h3>Statistics Page:</h3>
-            The statistics page is consisted of 3 important sections: The Chart, the Table, and the Stats.
-            <h4>Table</h4>
-            In the table, you have a good overview of all of your times. You can also quickly toggle between OK, +2, and DNF penalties. There is also a measure of ao5, ao12, and mo3, updated at each solve during your session.
-
-            <h4>Chart</h4>
-            In the chart, you can see a trend in your times. It also looks very nice!
-
-            <h4>Stats</h4>
-            In stats, you can see your best and current single, ao5, ao12, and mo3. You can also see the standard deviation.
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </div>
-    )
+        <div>
+            <div className={styles.settingsPage}>
+                <Gate>
+                    <h1 className={styles.helpTitle}>Help</h1>
+                    <Tabs defaultIndex={settings.currentHelpTab} onSelect={(index) => {
+                        dispatch(setHelpTab(index))
+                    }}>
+                        <TabList>
+                            <Tab>General</Tab>
+                            <Tab>Main Page</Tab>
+                            <Tab>Sessions</Tab>
+                            <Tab>Widgets</Tab>
+                            <Tab>Statistics</Tab>
+                            <Tab>Settings</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <GeneralTab />
+                        </TabPanel>
+                        <TabPanel>
+                            <MainPageTab />
+                        </TabPanel>
+                        <TabPanel>
+                            <SessionsTab />
+                        </TabPanel>
+                        <TabPanel>
+                            <WidgetsTab />
+                        </TabPanel>
+                        <TabPanel>
+                            <StatisticsTab />
+                        </TabPanel>
+                        <TabPanel>
+                            <SettingsTab />
+                        </TabPanel>
+                    </Tabs>
+                </Gate>
+            </div>
+        </div >
+    );
 }
---
+++ app/(others)/help/tabs/mainPageTab.js
@@ -0,0 +1,22 @@
+import styles from "../help.module.css"
+export default function MainPageTab() {
+    return (
+        <div className={styles.infoTab}>
+            <h2>Introduction</h2>
+            The main page, reachable with the first icon in the top menu, or alternatively reachable by clicking the logo, contains everything you need for timing solves, viewing current averages, and more.
+            <h2>Timing</h2>
+            To start the timer, use the spacebar. When you first press down the spacebar, the time will turn red. Wait until it flashes into another color (your Highlight Color, if you set it in settings) and then release the spacebar to start the timer. During the solve, hit the Escape button to exit the solve without recording the time. Hit any other button to stop the timer and save the time. Now, you will be sent into judging mode.
+            <h2>Judging</h2>
+            In judging mode, you can go straight into another solve with the spacebar. However, if you decide that your solve needs to be penalized with a +2 or DNF, simply hit the corresponding buttons below the time. Alternatively, to delete the time, hit the delete button. Judging can also be done in the Statistics page afterwards.
+            <h2>Action Bar</h2>
+            Above the time you will see your scramble. Above the scramble, you will see the Action Bar, containing Quick Settings for events and input method. Feel free to choose the event of your choice, and the scramble will change depending on your event. Alternatively, feel free to choose how you enter your times, with the keyboard or Stackmat.
+            <h2>Data Persist</h2>
+            Data Persist is a special feature in Cronix, built directly into the core of the timer. Nearly every aspect of the timer is saved in browser cache, allowing the scramble, current judging status, and more to be saved. This means that across refresh and/or leaving and restoring the page, Cronix will be exactly as you left it, without any extra work.
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+    )
+}
